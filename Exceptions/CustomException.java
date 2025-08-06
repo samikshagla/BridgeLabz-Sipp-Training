68491065abcf08f64924ec6f4ba40ed1c355ec99
samikshagla <samikshachauhan124@gmail.com>
2025-08-06 10:31:08 +0530

    [Samiksha Chauhan].Add Exceptions

---
+++ Exceptions/CustomException.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class CustomException {
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+            System.out.print("Enter your age: ");
+            int age = scanner.nextInt();
+            
+            validateAge(age);
+            System.out.println("Access granted!");
+            
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+} 
